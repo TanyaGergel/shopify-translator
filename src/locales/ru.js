dc4e4ba146c8b2177afb17d2dec62f9c53245f21
TanyaGergel <project@genovawebart.com>
2017-05-29 14:42:32 +0300

    Create ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,268 @@
+if (typeof LANGUAGES === 'undefined') { var LANGUAGES = {} }
+
+LANGUAGES['fr'] = {
+  'general': {
+    'sections': {
+      'home': 'Tableau de bord',
+      'orders': 'Commandes',
+      'drafts': 'Pré-commandes',
+      'checkouts': 'Abandons',
+      'abandoned-checkouts': 'Checkouts abandonés',
+      'products': 'Produits',
+      'customers': 'Clients',
+      'reports': 'Rapports',
+      'discounts': 'Promotions',
+      'sales-channels': 'Canaux de vente',
+      'channels': 'Canaux de vente',
+      'online_store': 'Boutique en ligne',
+      'buy_button': 'Buy Button',
+      'mobile_app': "App mobile",
+      'pos': "Points de vente",
+      'apps': "Applications",
+      'settings': "Configuration",
+      'online_preferences': "Préférences",
+      'themes': "Thèmes",
+      'blogs': "Blogues",
+      'pages': "Pages",
+      'navigation': "Navigation",
+      'domains': "Domaines",
+      'inventory': "Inventaire",
+      'inventory_transfers': "Transferts",
+      'collections': "Collections",
+      'gift_cards': "Cartes Cadeaux",
+      'files': "Fichiers",
+      'taxes': "Taxes",
+      'checkout': "Paniers",
+      'general': "Général",
+      'payments': "Paiements",
+      'shipping': "Livraison",
+      'notifications': "Notifications",
+      'plan': "Profile",
+      'account': "Profile",
+    },
+
+    'search': {
+      'placeholder': 'Recherche',
+    },
+
+    'buttons': {
+      'export': 'Exporter',
+      'import': 'Importer',
+      'print': 'Imprimer',
+      'save': 'Sauvegarder',
+      'edit': 'Éditer',
+      'cancel': 'Annuler',
+      'change': 'Changer',
+      'destroy': "Détruire",
+      'post': 'Envoyer',
+      'filter': 'Filtrer'
+    }
+  },
+
+  'modal': {
+    'titles': {
+      'capture-payment': "Capturer un paiement",
+    },
+  },
+
+  'shared': {
+    'customer-card': {
+      'customer': 'Client',
+      'shipping-address': 'Adresse de livraison',
+      'billing-address': 'Adresse de facturation',
+      'no-address-set': 'Aucune adresse',
+      'same-as-shipping-address': "Identique à l'adresse de livraison",
+      'accepts-marketing': "Accepte le marketing",
+      'has-an-account': "Possède un compte",
+      'contact': "Contact",
+      'view-map': "Voir sur la carte",
+    },
+  },
+
+  'home-index': {
+    'cards': {
+      'api_buy_button_create_first_embed_card': '',
+      'multi_image_buy_button_announcement_card': '',
+    },
+
+    'today-features': {
+      "todays-total-sales": "Total des ventes aujourd'hui",
+      "todays-visits": "Nombre de visites aujourd'hui",
+      "todays-orders": "Nombre de commandes aujourd'hui",
+    },
+
+    'titles': {
+      'total-sales': "Total des ventes",
+      'top-products': "Meilleurs produits",
+    },
+
+    'buttons': {
+      'all-channels': "Tous les cannaux",
+      'online-store': "Boutique en ligne",
+      'view-order': "Voir la commande",
+      'view-orders': "Voir les commandes",
+      'create-button': "Créer un Buy Button",
+      'create-buy-button': "Créer un Buy Button",
+      'learn-more': "En apprendre plus"
+    }
+  },
+
+  'products-index': {
+    'table': {
+      'tabs': {
+        'all-products': "Tous les produits",
+      },
+    },
+
+    'buttons': {
+      'add-product': "Ajouter un produit",
+    },
+  },
+
+  'orders-index': {
+    'table': {
+      'tabs': {
+        'all-orders': "Toutes les commandes",
+        'open': "Ouvertes",
+        'unfulfilled': "Remplies",
+        'unpaid': "Non payées",
+      },
+
+      'columns': {
+        'order': 'Commande',
+        'date': 'Date',
+        'customer': 'Client',
+        'payment-status': 'Payment',
+        'fulfillment-status': 'Statut',
+        'total': 'Total',
+      },
+
+      'badges': {
+        'fulfilled': 'Complétée',
+        'unfulfilled': 'Non complétée',
+        'paid': 'Payée',
+        'pending': 'En attente',
+        'refunded': 'Remboursée',
+        'authorized': "Authorisée"
+      },
+    },
+
+    'filters': {
+      'status': "Statut",
+      'credit-card': "Carte de crédit",
+      'customer': "Client",
+      'payment-status': "Payment",
+      'fulfillment-status': "Statut de complétion",
+      'tagged-with': "Avec tags...",
+      'risk-level': "Niveau de risque",
+    },
+
+    'buttons': {
+      'create-order': "Ajouter une commande",
+    },
+  },
+
+  'orders-show': {
+    'buttons': {
+      'print': "Imprimer",
+      'cancel-order': "Canceller",
+      'archive': "Archiver",
+      'view-order-status-page': "Voir la page de statut de la commande",
+      'capture-payment': "Capturer",
+      'restock': "Restaurer l'inventaire",
+      'fulfill-items': "Compléter",
+      'resend-email': "Réenvoyer le courriel",
+      'mark-as-paid': "Marquer comme payé",
+    },
+
+    'titles': {
+      'order-details': 'Détails',
+      'conversion-details': 'Détails de conversion',
+      'unfulfilled': 'Non complétés',
+      'accept-payment': 'Paiement',
+      'fulfill-items': 'Compléter',
+      'risk-level': 'Niveau de risque',
+      'customer': 'Client',
+      'tags': 'Tags',
+      'order-contact': 'Contact',
+      'timeline': 'Chronologie',
+      'payment-pending': 'Paiement en attente',
+    },
+
+    'texts': {
+      'show-comments': "Montrer les commentaires",
+    },
+  },
+
+  'checkouts-index': {
+    'table': {
+      'tabs': {
+        'all-checkouts': "Tous les checkouts",
+      },
+
+      'columns': {
+        'checkout': 'Commande',
+        'date': 'Date',
+        'placed-by': 'Client',
+        'email-status': 'Courriel',
+        'recovery-status': 'Statut',
+        'total': 'Total',
+      },
+
+      'badges': {
+        'sent': 'Envoyé',
+        'recovered': 'Récupéré',
+        'not-recovered': 'Non récupéré',
+        'not-sent': 'Non envoyé',
+      },
+    },
+  },
+
+  'checkouts-show': {
+    'buttons': {
+      'send-a-cart-recovery-email': 'Envoyer un courriel'
+    },
+  },
+
+  'customers-show': {
+    'titles': {
+      'recent-orders': "Commandes récentes",
+      'default-address': "Addresse principale",
+      'contact': "Information",
+    },
+  },
+
+  'draft-orders-index': {
+    'buttons': {
+      'create-order': 'Créer une commande'
+    },
+
+    'empty': {
+      'title': 'Créer des commandes et factures',
+      'subtitle': 'Acceptez des commandes au téléphone, envoyez des factures par courriel et acceptez des paiements.'
+    }
+  },
+
+  'draft': {
+    'buttons': {
+      'save-draft-order': 'Sauvegarder la commande',
+      'browse-products': "Parcourir les produits",
+      'add-custom-item': "Ajouter un article personnalisé",
+      'view-all-tags': "Voir tous les tags",
+      'add-discount': "Ajouter un rabais",
+      'add-shipping': "Ajouter du shipping",
+      'taxes': "Taxes",
+      'email-invoice': "Envoyer",
+      'mark-as-paid': "Marquer comme payé",
+      'mark-as-pending': "Marquer comme en attente",
+      'pay-with-credit-card': "Payer par carte de crédit",
+    },
+
+    'titles': {
+      'order-details': 'Détails',
+      'find-or-create-a-customer': 'Trouver ou créer un client',
+      'email-invoice': 'Envoyer la facture par courriel',
+      'accept-payment': 'Paiement',
+    },
+  },
+}
